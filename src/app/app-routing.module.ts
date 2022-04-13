d3b204da977dd469bb7a150b6a21db71754ba9a6
javierfurus <javierfurus@gmail.com>
2022-04-13 14:26:53 +0200

    2 files modified, 2 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormComponent } from './form/form.component';
 import { HomeComponent } from './home/home.component';
 import { TestComponentComponent } from './test-component/test-component.component';
 import { UserFeedComponent } from './user-feed/user-feed.component';
@@ -9,6 +10,7 @@ const routes: Routes = [
   { path: 'test', component: TestComponentComponent },
   { path: 'user-feed', component: UserFeedComponent },
   { path: '', component: HomeComponent },
+  { path: '', component: FormComponent },
 ];
 
 @NgModule({
